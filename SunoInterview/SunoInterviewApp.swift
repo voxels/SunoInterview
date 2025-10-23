18cf7a899f583098408cb24fa46aa3c31166a9ad
voxels <voxels@gmail.com>
2025-10-23 12:11:44 -0400

    Initial Commit

---
+++ SunoInterview/SunoInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunoInterviewApp.swift
+//  SunoInterview
+//
+//  Created by Michael A Edgcumbe on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SunoInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
